d84462093dd0cf835e1be91e6fab8b472135fd35
BRajendra10 <rajendrabehera8116@gmail.com>
2025-11-09 18:54:05 +0530

    file & folders setup for new project youtube-clone, with react, tailwind-css, shadcn

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="w-full h-screen bg-red-500 flex justify-center items-center text-4xl uppercase">App</div>
+  )
+}
+
+export default App
